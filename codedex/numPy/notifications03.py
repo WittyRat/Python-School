10ce17408275fb87616b1b50be50304774ccfc4d
WittyRat <127035631+WittyRat@users.noreply.github.com>
2025-11-21 10:32:14 +0200

    Rearranged files

---
+++ codedex/numPy/notifications03.py
@@ -0,0 +1,14 @@
+"""Studies have found that people now get as many as 237 notifications every day on their smartphones. 🔔 Let’s see how many you got!
+
+Look through your settings:
+
+iPhone: Settings > Screen Time > See All App & Website Activity > Notifications (bottom)
+Android: Settings > Digital Wellbeing and parental controls > Notifications received
+Create an array of the number of notifications you received in the past week.
+
+Print the array out."""
+import numpy as np
+
+notifications = np.array([10, 89, 53, 57, 58])
+
+print(notifications)
